4f43920c82d6263bd1b4c6df1ccc67cf70f1bf47
Daniel Cheeseman <gridlfood@gmail.com>
2025-12-01 15:05:32 +0000

    Remove verification

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -52,8 +52,10 @@
             <div class="col-md-4 phone">
               <a href="tel:07922 214361">
                         <span class="icon">
-                            <i class="fa-solid fa-circle-phone-hangup"></i>
-                        </span>
+                           <svg fill="#cbba57" width="60" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.0.0-alpha3 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path
+                               d="M256 0c-141.4 0-256 114.6-256 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM419.1 256.1L392.5 299.9c-3.988 6.396-11.99 9.015-19.06 6.253l-54.82-21.92c-6.441-2.596-10.41-9.16-9.717-16.06l3.446-34.61C275.9 221.1 236.1 221.1 199.6 233.6l3.447 34.61c.7402 6.885-3.243 13.5-9.717 16.06L138.5 306.2c-7.037 2.797-15.1 .1003-19.06-6.253L92.05 256.1C88.16 249.9 89.06 241.9 94.25 236.7c89.18-89.18 234.3-89.2 323.5 .0002C422.9 241.9 423.8 249.9 419.1 256.1z"/></svg>
+
+                        </span><br/><br>
                 <span>07922 214361</span>
               </a>
 
@@ -64,8 +66,8 @@
                 href="https://www.google.com/maps?gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7Mg0IARAuGK8BGMcBGIAEMgcIAhAAGIAEMgYIAxBFGDsyEwgEEC4YrwEYxwEYgAQYmAUYmQUyBggFEEUYPDIGCAYQRRg8MgYIBxBFGD3SAQg1MjY1ajBqN6gCALACAA&amp;um=1&amp;ie=UTF-8&amp;fb=1&amp;gl=uk&amp;sa=X&amp;geocode=KV0KWv9CemtIMZKmgmZ6Qkkg&amp;daddr=Pool+St,+Bodmin+PL31+2HA"
                 target="_blank">
                         <span class="icon">
-                        <i class="fa-solid fa-compass"></i>
-                        </span>
+<svg fill="#cbba57" width="60" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.0.0-alpha3 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M265.4 233.4c-12.5 12.5-12.5 32.75 0 45.25s32.75 12.5 45.25 0s12.5-32.77 0-45.25C298.1 220.9 277.9 220.9 265.4 233.4zM288 0C146.6 0 32 114.6 32 256c0 141.4 114.6 256 256 256s256-114.6 256-256C544 114.6 429.4 0 288 0zM414.1 156l-65.97 144.4c-2.719 5.969-9.778 13.02-15.75 15.76l-144.3 65.97c-16.66 7.609-33.81-9.547-26.19-26.2l65.97-144.3c2.719-5.984 9.781-13.05 15.78-15.78l144.3-65.97C404.6 122.3 421.8 139.4 414.1 156z"/></svg>
+                        </span><br><br>
                 <span>The White Hart<br>
                             <small>Pool St, Bodmin, PL31 2HA.</small>
                         </span>
@@ -74,8 +76,9 @@
             <div class="col-md-4 email">
               <a href="mailto:info@theh.art">
                         <span class="icon">
-                            <i class="fa-solid fa-circle-envelope"></i>
-                        </span>
+<svg fill="#cbba57" width="60" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.0.0-alpha3 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path
+    d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM384 320c0 17.67-14.33 32-32 32H160c-17.67 0-32-14.33-32-32V238l122.1 48.82C251.1 287.6 253.1 288 256 288s4.031-.375 5.938-1.141L384 238V320zM384 203.6l-128 51.2L128 203.6V192c0-17.67 14.33-32 32-32h192c17.67 0 32 14.33 32 32V203.6z"/></svg>
+                        </span><br/><br>
                 <span>bodmin@theh.art</span>
               </a>
             </div>
